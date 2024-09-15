7a84ae3cbb5554f27971749a45fb4f98ec1d0113
bizmapNikhil <nikhil.tulankar@bizmap.in>
2024-09-15 19:21:32 +0200

    resolve the issue for not applicable customer

--- india_tcs_206c_1h/india_tcs_206c_1h/doctype/india_tcs_206c_1h_app_settings/india_tcs_206c_1h_app_settings.py
+++ india_tcs_206c_1h/india_tcs_206c_1h/doctype/india_tcs_206c_1h_app_settings/india_tcs_206c_1h_app_settings.py
@@ -8,41 +8,53 @@ from frappe.model.document import Document
 from frappe.utils import flt, cint
 
 class IndiaTCS206C_1HAppSettings(Document):
-	pass
+    pass
 
 def set_tcs(doc, action):
-	if action == "validate":
-		settings = frappe.get_single("India TCS 206C_1H App Settings")
-		if not cint(settings.enable_tcs):
-			return
-		if doc.docstatus != 0:
-			return
-		if len(doc.taxes) < 1:
-			return
-		if doc.taxes[-1].account_head == settings.tcs_account:
-			return
-		from erpnext.accounts.party import get_dashboard_info
-		info = get_dashboard_info("Customer", doc.customer)
-		customer_turnover = 0
-		for company_info in info:
-			if company_info['company'] == doc.company:
-				customer_turnover = company_info['billing_this_year']
-		if settings.tcs_trigger_amount >= customer_turnover + flt(doc.rounded_total):
-			return
-		tcs_row = frappe.new_doc('Sales Taxes and Charges')
-		tcs_row.update({
-						"charge_type": "On Previous Row Total",
-						"row_id": str(len(doc.taxes)),
-						"account_head": settings.tcs_account,
-						"rate": settings.tcs_percentage,
-						"description": settings.tcs_invoice_description,
-						"parenttype": "Sales Invoice",
-						"parentfield": "taxes",
-						"parent": doc.name,
-						"idx": len(doc.taxes) + 1
-					})
-		doc.taxes.append(tcs_row)
-		from erpnext.controllers.taxes_and_totals import calculate_taxes_and_totals
-		calculate_taxes_and_totals(doc)
+    if action == "validate" and not doc.is_return:
+        from erpnext.controllers.taxes_and_totals import calculate_taxes_and_totals
+        settings = frappe.get_single("India TCS 206C_1H App Settings")
+        if not cint(settings.enable_tcs):
+            return
+        if cint(settings.check_tcs_for_customer):
+            if not frappe.db.get_value("Customer", doc.customer, "tax_withholding_category"):
+                to_remove = []
+                for d in doc.get("taxes"):
+                    if d.account_head == settings.tcs_account:
+                        to_remove.append(d)
+                [doc.remove(d) for d in to_remove]
+                calculate_taxes_and_totals(doc)
+                return
 
-
+        if doc.docstatus != 0:
+            return
+        if len(doc.taxes) < 1:
+            return
+        if doc.is_return == 1:
+            return
+        if doc.taxes[-1].account_head == settings.tcs_account:
+            return
+        from erpnext.accounts.party import get_dashboard_info
+        info = get_dashboard_info("Customer", doc.customer)
+        customer_turnover = 0
+        for company_info in info:
+            if company_info['company'] == doc.company:
+                customer_turnover = company_info['billing_this_year']
+        if settings.tcs_trigger_amount >= customer_turnover + flt(doc.rounded_total):
+            return
+        tcs_row = frappe.new_doc('Sales Taxes and Charges')
+        tcs_row.update({
+            "charge_type": "On Previous Row Total",
+            "row_id": str(len(doc.taxes)),
+            "account_head": settings.tcs_account,
+            "rate": settings.tcs_percentage,
+            "description": settings.tcs_invoice_description,
+            "parenttype": "Sales Invoice",
+            "parentfield": "taxes",
+            "parent": doc.name,
+            "idx": len(doc.taxes) + 1
+        })
+        # Add a debug log to check if the charge_type is being set correctly
+        frappe.log_error("TCS Row Update: {}".format(tcs_row.as_dict()))
+        doc.taxes.append(tcs_row)
+        calculate_taxes_and_totals(doc)
